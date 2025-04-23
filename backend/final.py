af379a3c73a4a7516f32356555b41cf379464db3
KrutagnaKathiya <Krutagna28@gmail.com>
2025-04-23 12:08:04 +0530

    added background audio with looping feature

--- backend/final.py
+++ backend/final.py
@@ -11,9 +11,9 @@ video_path = "uploads/test.mp4"  # Replace with your video path
 cap = cv2.VideoCapture(video_path)
 
 # Get frames per second (fps) of the video
-print("Fps:",cap.get(cv2.CAP_PROP_FPS))
+print("Fps:", cap.get(cv2.CAP_PROP_FPS))
 cap.set(cv2.CAP_PROP_FPS, 60)
-fps = int(cap.get(cv2.CAP_PROP_FPS))  
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 
 
 # Check if video opened successfully
@@ -27,45 +27,44 @@ saved_count = 0
 list = []
 while True:
     ret, frame = cap.read()
-    
+
     if not ret:
         break  # Break loop if no frame is read (end of video)
 
     # Save only one frame per second
-    if i % fps == 0:  
-        print(i,"\n")
-        print("Status",i/cap.get(cv2.CAP_PROP_FRAME_COUNT)*100,"%")
+    if i % fps == 0:
+        print(i, "\n")
+        print("Status", i / cap.get(cv2.CAP_PROP_FRAME_COUNT) * 100, "%")
         # Resize frame to 500x500
-        frame = cv2.resize(frame, (416,416))
+        frame = cv2.resize(frame, (416, 416))
 
-       
         # cv2.imwrite("img11.png", frame)
         # time.sleep(1)
         result = model(frame, conf=0.7, verbose=False)
-        
+
         # print(result[0].boxes)
         # break
         box = result[0].boxes.cls.tolist()
         # Print results
         # print(box)
 
-        if(len(box)==2):
+        if len(box) == 2:
             list.append(int(j))
-        elif(len(box)==1 and box[0]==1.0):
+        elif len(box) == 1 and box[0] == 1.0:
             list.append(int(j))
         j += 1
-       
-        
 
     i += 1  # Increment total frame count
 
 # Release video
 cap.release()
-#======================================================================================
-#Code for making timestamps
+
+
+# ======================================================================================
+# Code for making timestamps
 def solve(test):
     ans = []
-    
+
     for i in range(len(test)):
         if i == 0:
             temp = [max(0, test[i] - 6), test[i]]
@@ -73,31 +72,29 @@ def solve(test):
         else:
             a = max(0, test[i] - 6)
             b = test[i]
-            
+
             # Merge intervals if overlapping
             if a <= ans[-1][1]:
                 ans[-1][1] = b
             else:
                 ans.append([a, b])
-    
+
     return ans
 
 
+clips = []
+# path = "D:\hackthon\Valorant Gameplay.mp4"
 
-
-clips =[]
-#path = "D:\hackthon\Valorant Gameplay.mp4"
-    
 if len(list) == 0:
     print("There is no kills in this game")
 else:
     ans = solve(list)
     k = 0
     for interval in ans:
-        clips.append(VideoFileClip(video_path).subclipped(interval[0],interval[1]))
+        clips.append(VideoFileClip(video_path).subclipped(interval[0], interval[1]))
         print(interval[0], interval[1])
 
-#========================================================
+# ========================================================
 # print(list)
 
 
@@ -112,45 +109,39 @@ else:
 video = concatenate_videoclips(clips)
 # video.write_videofile("final/output.mp4")
 # from moviepy import *
-# from moviepy.audio.fx.all import volumex 
+# from moviepy.audio.fx.all import volumex
 
 
 # video_path = "final/temp.mp4"
 
 audio_path = ""
 
+# adding background audio to highlight video
 
-if(sys.argv[1]=="1"):
+if sys.argv[1] == "1":
     audio_path = "uploads/audio.mp3"
     print(audio_path)
     original_audio = video.audio
-    video_time=video.duration
+    video_time = video.duration
 
-    audio=AudioFileClip(audio_path)
-    audio1=audio
+    audio = AudioFileClip(audio_path)
+    audio1 = audio
+    # looping audio if audio lenght is less then video lenght
+    while audio.duration - 25 < video_time:
+        audio = audio1 + audio
 
-    while(audio.duration-25 < video_time):
-        audio=audio1+audio
-
-
-    music = audio.subclipped(25, 25+video_time)
+    music = audio.subclipped(25, 25 + video_time)
 
     music = music.with_effects([afx.MultiplyVolume(0.3)])
 
-
-
     final_audio = CompositeAudioClip([original_audio, music])
     video = video.with_audio(final_audio)
-    print("Audio file found===================================================================================================================")
-
-
+    print(
+        "Audio file found==================================================================================================================="
+    )
 
 
 video.write_videofile("final/output.mp4", codec="libx264", audio_codec="aac")
-
-
-
-
 
 
 # original_audio = video.audio
@@ -159,11 +150,9 @@ video.write_videofile("final/output.mp4", codec="libx264", audio_codec="aac")
 # music = AudioFileClip(audio_path).subclipped(25, 25+video_time)
 
 # music = music.with_effects([afx.MultiplyVolume(0.3)])
- 
 
 
 # final_audio = CompositeAudioClip([original_audio, music])
 # video = video.with_audio(final_audio)
 
-#video.write_videofile("final/output.mp4", codec="libx264", audio_codec="aac")
-
+# video.write_videofile("final/output.mp4", codec="libx264", audio_codec="aac")
